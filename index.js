a29bb91d8541a5fd3db54f7aa0c04221790c09c2
Sinan Demir <sinanndmrr@gmail.com>
2021-07-23 18:34:55 +0300

    ping komutu eklendi

--- index.js
+++ index.js
@@ -57,6 +57,31 @@ function embedOlustur(
 // KOMUTLAR
 
 // ************************************** //
+// Bot'un pingi
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'ping') {
+    msg.channel.send('**Gecikme süresi hesaplanıyor...**').then(sent => {
+      setTimeout(function () {
+        sent.delete();
+        msg.channel.send(
+          new Discord.MessageEmbed().setColor('#00ff6e').addFields(
+            {
+              name: ':grey_question: Websocket',
+              value: `:satellite: ${client.ws.ping} ms`,
+            },
+            {
+              name: ':grey_question:  Gecikme',
+              value: `:satellite: ${
+                sent.createdTimestamp - msg.createdTimestamp
+              } ms`,
+            }
+          )
+        );
+      }, 1000);
+    });
+  }
+});
+// ************************************** //
 // Müzik botu
 client.on('message', async msg => {
   const ytdl = require('ytdl-core');
@@ -193,13 +218,6 @@ client.on('message', msg => {
 
     msg.reply(`${duyuruKisi} kişisine "${duyuruMesaji}" duyurusu yapıldı.`);
     duyuruKisi.send(duyuruMesaji);
-  }
-});
-// ************************************** //
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === prefix + 'ping') {
-    // msg.reply('pong'); // Kişiyi etikelleyip, 'pong' yazdırıyoruz.
-    msg.channel.send('pong'); // Direkt sunucuya mesaj gönderiyoruz.
   }
 });
 // ************************************** //
